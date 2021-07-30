82d7aab2f6d802b8ea96486f152bbf648fe4d5a8
sumercetin <sumer@jotform.com>
2021-07-31 00:50:34 +0300

    Calculator :: Minor refactoring

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import { Calculator, Wrapper } from './style/styleCalculator';
 function App() {
   const [inputState, setInputState] = useState({ prevInput: '0', finalInput: '', operator: '' });
   const VALID_KEYCODES = [37, 43, 45, 47, 120];
+  const OPERATIONS = {
+    '+': (prevInput, currentInput) => prevInput + currentInput,
+    '-': (prevInput, currentInput) => prevInput - currentInput,
+    '/': (prevInput, currentInput) => prevInput / currentInput,
+    'x': (prevInput, currentInput) => prevInput * currentInput
+  }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleKeyClick = useCallback((e) => {
@@ -50,9 +56,7 @@ function App() {
   // Check if number or any operator is pressed, get mouse clicks otherwise
   const getPressedKey = (e) => (e.keyCode >= 48 && e.keyCode <= 57) || (VALID_KEYCODES.indexOf(e.keyCode) >= 0) ? e.key : e.target.innerText;
 
-  const setDefaultState = (pressedKey = '0') => {
-    setInputState({ prevInput: pressedKey, finalInput: '', operator: '' });
-  }
+  const setDefaultState = (pressedKey = '0') => setInputState({ prevInput: pressedKey, finalInput: '', operator: '' });
 
   const multiplyNegative = () => {
     const multipliedNum = '' + (parseFloat(inputState.prevInput) * -1);
@@ -60,29 +64,15 @@ function App() {
   }
 
   const getPercent = () => {
-    const percentNum = '' + (parseFloat(inputState.prevInput) /100);
+    const percentNum = '' + (parseFloat(inputState.prevInput) / 100);
     setInputState({ prevInput: percentNum, operator: '%' });
   }
 
-  const setOperator = pressedKey => {
-    setInputState({ ...inputState, operator: pressedKey });
-  }
+  const setOperator = pressedKey => setInputState({ ...inputState, operator: pressedKey });
 
   const performOperation = pressedKey => {
     if (inputState.operator && inputState.finalInput && inputState.operator !== '+/-' && inputState.operator !== '%') {
-      const currentInput = parseFloat(inputState.prevInput);
-      const prevInput = parseFloat(inputState.finalInput);
-
-      let calculatedVal;
-      if(inputState.operator === '+') {
-        calculatedVal = prevInput + currentInput;
-      } else if(inputState.operator === '-') {
-        calculatedVal = prevInput - currentInput;
-      } else if(inputState.operator === '/') {
-        calculatedVal = prevInput / currentInput;
-      } else if(inputState.operator === 'x') {
-        calculatedVal = prevInput * currentInput;
-      }
+      const calculatedVal = OPERATIONS[inputState.operator](parseFloat(inputState.finalInput), parseFloat(inputState.prevInput));
 
       setInputState({ prevInput: '' + calculatedVal, finalInput: '', operator: pressedKey });
     } else {
